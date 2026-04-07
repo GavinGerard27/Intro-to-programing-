bfd67cbe32d3f81821ba2867619e5ea0d6c7f76a
GavinGerard27 <828387@mystma.org>
2026-04-07 14:33:18 -0500

    mad lib

---
+++ Assignments/Simple_Mad_Lib.py
@@ -0,0 +1,13 @@
+# One[adjective] 
+# morning, i found a mysterious [noun]
+# in my backyard. i [Verb]
+# toward it [Adverb]
+# but suddenly it exploded into a pile of [Food]
+# !
+Adjective = input(" An adjective is")
+noun =input(" a noun is " )
+Verb=input(" a verb is " )
+Adverb=input("a adverb is")
+Food= input ("a food is")
+
+print("One" + Adjective + " morning, i found a mysterious " + noun + "in my backyard. I " +Verb + " toward it " + Adverb + "but suddenly it exploded into a pile of "  + Food " ! ")
