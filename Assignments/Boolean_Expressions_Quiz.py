d58854ec91fcc7c7aa1205afd6b0da5c61a8f5e9
GavinGerard27 <828387@mystma.org>
2026-04-10 14:43:26 -0500

    Simple quiz

---
+++ Assignments/Boolean_Expressions_Quiz.py
@@ -0,0 +1,12 @@
+Question_1 = int(input(" Give a number less than 5"))
+print(Question_1 < 5  )
+question_2= int((input(" Give a number less than 10")))
+print(Question_1 < 10)
+question_3 = int(input(" Give a number more than 10"))
+print(question_3 > 10)
+question_4 = int(input(" Give a number more than 5"))
+print(question_4 > 5)
+question_5 = int(input(" Give a number eaqual to 10"))
+print(question_5 == 10)
+
+
